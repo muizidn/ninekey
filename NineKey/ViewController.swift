d7a833fffe7a8d12559dc1cc97ab5a8c6594810e
Kunto I.K Jati <kuncoro8jati@gmail.com>
2019-06-01 05:52:33 +0700

    Initial Commit

---
+++ NineKey/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NineKey
+//
+//  Created by PondokIOS on 01/06/19.
+//  Copyright © 2019 PondokIOS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ NineKeyTests/NineKeyTests.swift
@@ -0,0 +1,34 @@
+//
+//  NineKeyTests.swift
+//  NineKeyTests
+//
+//  Created by PondokIOS on 01/06/19.
+//  Copyright © 2019 PondokIOS. All rights reserved.
+//
+
+import XCTest
+@testable import NineKey
+
+class NineKeyTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
